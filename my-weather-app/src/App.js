3440c2bf4d9ed26f49cb11b6e8fc76c97981e97e
Yohelise <yohelise.estaba@gmail.com>
2021-08-13 18:09:57 +0200

    maquetacion weather card left and integration to main app container

--- my-weather-app/src/App.js
+++ my-weather-app/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import BigWeatherCard from './Big-weather-card';
 import ComponentWeek from './ComoponentWeek'
 function App() {
   return (
-    <div >
-      <ComponentWeek></ComponentWeek>
+    <div className="app_container">
+    <div className= "total-container">
+    <BigWeatherCard></BigWeatherCard>
+    <div className="main-view"></div>
+    <ComponentWeek></ComponentWeek>
+    <div className="medium-weather-card"></div>
     </div>
+
+  </div>
   );
 }
 
